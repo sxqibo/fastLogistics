3c122dcd89be7dc8dec490f123d86cc6bbf930a6
Tian en <522203224@qq.com>
2020-12-04 14:25:57 +0800

    修改杰航产品数组bug

--- src/Jiehang.php
+++ src/Jiehang.php
@@ -129,8 +129,8 @@ class Jiehang
      * @param string $receiverPostCode 发件人邮编
      * @param string $receiverMobile 发件人手机号
      * @param array $goods 商品属性，二维数组， 有5个必填项，包裹申报名称(中文)，包裹申报名称(英文)，申报数量，申报价格(单价)，申报重量(单重)
-     * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return mixed
      */
     public function createOrder(
         $orderNo, $channelCode,
@@ -156,9 +156,15 @@ class Jiehang
                 'CustomerNumber' => $orderNo,               //客户订单号(可传入贵公司内部单号)
                 'ChannelCode'    => $channelCode,           //渠道代码
                 'CountryCode'    => $receiverCountryCode,   //国家二字代码
-                'TotalWeight'    => array_sum(array_map(function ($val) {return ($val['goods_number'] * $val['goods_single_weight']);}, $goods)),  //订单总重量
-                'TotalValue'     => array_sum(array_map(function ($val) {return ($val['goods_number'] * $val['goods_single_worth']);}, $goods)),   //订单总申报价值
-                'Number'         => array_sum(array_map(function ($val) {return ($val['goods_number']);}, $goods))  //件数
+                'TotalWeight'    => array_sum(array_map(function ($val) {
+                    return ($val['goods_number'] * $val['goods_single_weight']);
+                }, $goods)),  //订单总重量
+                'TotalValue'     => array_sum(array_map(function ($val) {
+                    return ($val['goods_number'] * $val['goods_single_worth']);
+                }, $goods)),   //订单总申报价值
+                'Number'         => array_sum(array_map(function ($val) {
+                    return ($val['goods_number']);
+                }, $goods))  //件数
             ]
         ];
 
@@ -185,12 +191,12 @@ class Jiehang
         $data['Sender'] = [];
 
         // step1.9:（参数）订单明细产品信息
-        $data['OrderItems'];   //array, 申报信息
+        $data['OrderItems'] = [];   //array, 申报信息
         foreach ($goods as $k => $v) {
             $data['OrderItems'][$k]['Enname'] = $v['goods_en_name'];        //string,包裹申报名称(英文)必填
             $data['OrderItems'][$k]['Cnname'] = $v['goods_cn_name'];        //string,包裹申报名称(中文)，不必填
             $data['OrderItems'][$k]['Num']    = $v['goods_number'];         //int,申报数量,必填
-            $data['OrderItems'][$k]['Price']  = $v['goods_single_worth'] ;         //decimal( 18,2),申报价格(单价),必填
+            $data['OrderItems'][$k]['Price']  = $v['goods_single_worth'];         //decimal( 18,2),申报价格(单价),必填
             $data['OrderItems'][$k]['Weight'] = $v['goods_single_weight'];  //decimal( 18,3),申报重量(单重)，单位 kg,,必填
         }
 
